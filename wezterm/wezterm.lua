3854b21405ea5d54506413a8c715bc66c9560280
thinkgo <thinkgo@aliyun.com>
2025-04-16 16:06:24 +0800

    feat: add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Noto Sans CJK SC",
+})
+-- config.color_scheme = "Batman"
+config.color_scheme = "iTerm2 Dark Background"
+
+config.window_background_opacity = 0.85
+config.default_cursor_style = "BlinkingBar"
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 5,
+	bottom = 5,
+}
+
+config.leader = { key = "m", mods = "CTRL", timeout_millisends = 1000 }
+-- key binding
+config.keys = {
+	{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+for i = 1, 6 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
+
+return config
